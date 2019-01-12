557557e4b726523c9db7d556e655f88258dec73d
anjith <anjithpadmanabhan@gmil.com>
2019-01-12 19:39:01 +0530

    added

---
+++ wifi_ethernet.py
@@ -0,0 +1,34 @@
+import os
+#cheking for ping
+hostname = "google.com"
+response = os.system("ping -c 1 " + hostname)
+if response == 0:
+
+    print ("NETWORK ACTIVE")
+	
+else:
+	print ("PING NOT AVAILABLE")
+	#Reading the current connection
+	f = open("NET.txt","r+")
+	if f.mode == "r+":
+		c = f.readline(1)
+		if c =="E":
+			#Disabling Ethernet
+			os.system('sudo ifconfig enxfcde56ff0106 down')
+			#Enabling WIFI
+			os.system('rfkill unblock wlan')
+			f.seek(0)
+			f.truncate(0)
+			f.write("W")
+			print ("CONNECTING TO WIFI")
+
+		elif c =="W":
+			#Disabling WIFI
+			os.system('rfkill block wlan')
+			#Enabling Ethernet
+	    		os.system('sudo ifconfig enxfcde56ff0106 up')
+			f.seek(0)
+			f.truncate(0)
+			f.write("E")
+			print ("CONNECTING TO ETHERNET")
+		 
